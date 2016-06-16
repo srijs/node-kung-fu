1c50729e1d117f07f045233ac94a2e6c3faeaab9
Sam Rijs <srijs@airpost.net>
2016-06-16 21:23:02 +1000

    remove List

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,6 @@ import {Option} from './option';
 import {Either} from './either';
 import {Pair} from './pair';
 import {Unit} from './unit';
-import {List} from './list';
 import {PartialFunction} from './partial-function';
 
 export {
@@ -10,6 +9,5 @@ export {
   Either,
   Pair,
   Unit,
-  List,
   PartialFunction
 }
